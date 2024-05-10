baba737b70706eb7016ba719f7a3e8d0499370df
Eddie <eddie.freeman@streem.com>
2024-05-09 18:09:49 -0700

    make the bot more unique

--- owlsee_bot/main.py
+++ owlsee_bot/main.py
@@ -3,14 +3,19 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from dotenv import load_dotenv
+from openai import OpenAI
 
 load_dotenv()  # take environment variables from .env.
 
 tg_bot_token = os.getenv("TG_BOT_TOKEN")
+openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 messages = [
-    {"role": "system", "content": "You are a helpful assistant that answers questions."}
+    {
+        "role": "system",
+        "content": "You are an incredibly eloquent assistant to a space outlaw that answers questions with a jovial sense of humor, unending optimism, and snarky sarcasm sometimes referencing how everything is made of stardust.",
+    }
 ]
 
 logging.basicConfig(
@@ -24,10 +29,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    messages.append({"role": "user", "content": update.message.text})
+    completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo", messages=messages, temperature=0.8
+    )
+    completion_answer = completion.choices[0].message
+    messages.append(completion_answer)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=completion_answer.content
+    )
+
+
 if __name__ == "__main__":
     application = ApplicationBuilder().token(tg_bot_token).build()
 
     start_handler = CommandHandler("start", start)
+    chat_handler = CommandHandler("chat", chat)
     application.add_handler(start_handler)
+    application.add_handler(chat_handler)
 
     application.run_polling()
