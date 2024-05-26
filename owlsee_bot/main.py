6b46d802f977c3d5860ac3bd4b2f5f34f6121596
Eddie <eddie.freeman@streem.com>
2024-05-25 17:37:18 -0700

    add voice transcription

--- owlsee_bot/main.py
+++ owlsee_bot/main.py
@@ -144,11 +144,27 @@ async def image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response = openai.images.generate(
         prompt=update.message.text, model="dall-e-3", n=1, size="1024x1024"
     )
+    await update.message.reply_text("Sending prompt, generating image")
     image_url = response.data[0].url
     image_response = requests.get(image_url)
+    await update.message.reply_text("Voice note downloaded, transcribing now")
     await context.bot.send_photo(
         chat_id=update.effective_chat.id, photo=image_response.content
     )
+
+
+async def transcribe_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Make sure we have a voice file to transcribe
+    voice_id = update.message.voice.file_id
+    if voice_id:
+        file = await context.bot.get_file(voice_id)
+        await file.download_to_drive(f"voice_note_{voice_id}.ogg")
+        await update.message.reply_text("Voice note downloaded, transcribing now")
+        audio_file = open(f"voice_note_{voice_id}.ogg", "rb")
+        transcript = openai.audio.transcriptions.create(
+            model="whisper-1", file=audio_file
+        )
+        await update.message.reply_text(f"Transcript finished:\n {transcript.text}")
 
 
 if __name__ == "__main__":
@@ -158,7 +174,9 @@ if __name__ == "__main__":
     image_handler = CommandHandler("image", image)
     chat_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), chat)
     mozilla_handler = CommandHandler("mozilla", mozilla)
-
+    # This handler will be triggered for voice messages
+    voice_handler = MessageHandler(filters.VOICE, transcribe_message)
+    application.add_handler(voice_handler)
     application.add_handler(image_handler)
     application.add_handler(mozilla_handler)
     application.add_handler(start_handler)
